a2868a94710b653b888d146b54b86fadcaf14494
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-01-31 12:45:41 +0000

    add deposit method - adds amount to the initial balance

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -5,6 +5,10 @@ class BankAccount{
 		this.balance = 0;
 	}
 
+	deposit(amount){
+		this.balance += amount;
+	}
+
 }
 
 module.exports = BankAccount;
--- tests/bankAccount.test.js
+++ tests/bankAccount.test.js
@@ -16,4 +16,19 @@ describe("BankAccount", ()=>{
 		expect(subject.balance).toEqual(0)
 	})
 
+	describe("::deposit", ()=>{
+
+		it("has a deposit() instance method", ()=>{
+			expect(typeof subject.deposit).toBe("function")
+		})
+
+		it("deposits the amount indicated as a parameter", ()=>{
+			subject.deposit(500);
+			expect(subject.balance).toEqual(500)
+		})
+
+	})
+
+	
+
 })
