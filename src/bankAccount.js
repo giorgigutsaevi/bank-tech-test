7abdede8179b50b7d7358b83f54623c14a6a0e0d
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-02-01 12:38:38 +0000

    Add eslint and fixed all the errors, all 20 tests pass, 100% coverage

--- src/bankAccount.js
+++ src/bankAccount.js
@@ -1,45 +1,44 @@
-const BankOperation = require("./bankOperation");
+/* eslint-disable new-cap */
+const BankOperation = require('./bankOperation');
 
-class BankAccount{
+class BankAccount {
+  constructor(bankOperation = BankOperation) {
+    const STARTING_BALANCE = 0;
+    this.balance = STARTING_BALANCE;
+    this.operationsArray = [];
+    this.bankOperation = bankOperation;
+    this.STATEMENT_HEADER = 'date || credit || debit || balance\n';
+  }
 
-	constructor(bankOperation = BankOperation){
-		const STARTING_BALANCE = 0;
-		this.balance = STARTING_BALANCE;
-		this.operationsArray = [];
-		this.bankOperation = bankOperation;
-		this.STATEMENT_HEADER = "date || credit || debit || balance\n"
-	}
+  deposit(amount) {
+    this.balance += amount;
+    this.addOperation({ credit: amount, balance: this.balance });
+    return `${amount} is now deposited to your account!`;
+  }
 
-	deposit(amount){
-		this.balance += amount;
-		this.addOperation({credit: amount, balance: this.balance});
-		return `${amount} is now deposited to your account!`
-	}
+  withdraw(amount) {
+    if (this.balance <= 0 || amount > this.balance) {
+      return `Can't withdraw, please add more funds. Current balance: ${this.balance}`;
+    }
+    this.balance -= amount;
+    this.addOperation({ debit: amount, balance: this.balance });
+    return `${amount} is now withdrawn from your account!`;
+  }
 
-	withdraw(amount){
-		if(this.balance <= 0 || amount > this.balance){
-			return `Can't withdraw, please add more funds. Current balance: ${this.balance}`
-		}else{
-			this.balance -= amount;
-			this.addOperation({debit: amount, balance: this.balance});
-			return `${amount} is now withdrawn from your account!`
-		}
-	}
+  showBalance() {
+    return this.balance;
+  }
 
-	showBalance(){
-		return this.balance;
-	}
+  addOperation(obj) {
+    const operation = new this.bankOperation(obj);
+    this.operationsArray.unshift(operation);
+  }
 
-	addOperation(obj){
-		let operation = new this.bankOperation(obj)
-		this.operationsArray.unshift(operation)
-	}
-
-	printStatement(){
-		let statements = this.operationsArray.map(statement => statement.display())
-		console.log(`${this.STATEMENT_HEADER}${statements.join("\n")}`);
-	}
-
+  printStatement() {
+    const statements = this.operationsArray.map((statement) => statement.display());
+    // eslint-disable-next-line no-console
+    console.log(`${this.STATEMENT_HEADER}${statements.join('\n')}`);
+  }
 }
 
 module.exports = BankAccount;
--- src/bankOperation.js
+++ src/bankOperation.js
@@ -1,20 +1,19 @@
+class BankOperation {
+  constructor({ credit, debit, balance } = {}) {
+    this.date = new Date();
+    this.credit = credit;
+    this.debit = debit;
+    this.balance = balance;
+  }
 
-class BankOperation{
-	constructor({credit, debit, balance} = {}){
-		this.date = new Date();
-		this.credit = credit;
-		this.debit = debit;
-		this.balance = balance
-	}
-
-	display(){
-		return [
-			`${this.date.getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`,
-			this.credit,
-			this.debit,
-			this.balance,
-		].join(" || ")
-	}
+  display() {
+    return [
+      `${this.date.getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`,
+      this.credit,
+      this.debit,
+      this.balance,
+    ].join(' || ');
+  }
 }
 
 module.exports = BankOperation;
